f36da4a5e77bf1033bf81168a949acc06e05f1b5
SebastianCoppola <seba.coppola@hotmail.com>
2023-09-25 12:46:12 -0300

    update publicPath

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common.js');
 const packageJson = require('../package.json');
@@ -10,6 +9,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
