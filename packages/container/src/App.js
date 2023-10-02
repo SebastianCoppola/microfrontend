9c85902968653fd8d7818cdbf1ebb0f642c50c96
SebastianCoppola <seba.coppola@hotmail.com>
2023-10-02 19:23:35 -0300

    update style

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React, { lazy, Suspense, useState, useEffect } from 'react'
 import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
-import { StylesProvider, createGenerateClassName } from '@material-ui/core'
+import { StylesProvider, createGenerateClassName, Grid} from '@material-ui/core'
 import Header from './components/Header'
 import Progress from './components/Progress'
 
@@ -28,23 +28,27 @@ const App = () => {
     return (
         <Router history={history}>
             <StylesProvider generateClassName={generateClassName}>
-                <div>
-                    <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
-                    <Suspense fallback={<Progress />}>
-                        <Switch>
-                            <Route path='/auth'>
-                                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
-                            </Route>
-                            <Route path='/dashboard'>
-                                { !isSignedIn && <Redirect to='/'/> }
-                                <DashboardLazy />
-                            </Route>
-                            <Route path='/'>
-                                <MarketingLazy />
-                            </Route>
-                        </Switch> 
-                    </Suspense>
-                </div>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
+                    </Grid>
+                    <Grid item xs={12} justify='center'>
+                        <Suspense fallback={<Progress />}>
+                            <Switch>
+                                <Route path='/auth'>
+                                    <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                                </Route>
+                                <Route path='/dashboard'>
+                                    { !isSignedIn && <Redirect to='/'/> }
+                                    <DashboardLazy />
+                                </Route>
+                                <Route path='/'>
+                                    <MarketingLazy />
+                                </Route>
+                            </Switch> 
+                        </Suspense>
+                    </Grid>
+                </Grid>
             </StylesProvider>
         </Router>
     )
